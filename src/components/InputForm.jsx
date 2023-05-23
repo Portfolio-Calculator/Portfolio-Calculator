742feb2900366cb95b594c68757559b4eb2d5971
dmwcats <dmwcats123@gmail.com>
2023-05-22 23:32:58 -0500

    implemented stock allocation

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -4,8 +4,9 @@ import { useState } from "react";
 import Datepicker from 'react-tailwindcss-datepicker';
 
 const InputForm = () => {
-    const [symbols, setSymbols] = useState([]);
+    const [stockAllocations, setStockAllocations] = useState({}); 
     const [currSymbol, setCurrSymbol] = useState("");
+    const [currPercentage, setCurrPercentage] = useState("");
     const [initialBalance, setInitialBalance] = useState(null);
     const [selectedDate, setSelectedDate] = useState("");
 
@@ -15,6 +16,9 @@ const InputForm = () => {
 
     const handleSymbolChange = (event) => {
         setCurrSymbol(event.target.value);
+    }
+    const handlePercentageChange = (event) => {
+        setCurrPercentage(event.target.value);
     }
 
     const handleBalanceChange = (event) => {
@@ -26,8 +30,12 @@ const InputForm = () => {
     }
 
     const addStock = () => {
-        setSymbols((symbols) => [...symbols, currSymbol])
+        setStockAllocations(prevData => ({
+            ...prevData,
+            [currSymbol]: currPercentage
+        }));
         setCurrSymbol("");
+        setCurrPercentage("");
     }
 
     return(
@@ -35,15 +43,20 @@ const InputForm = () => {
             <label className = "w-1/2">
                 <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Symbol" type = "text" name = "symbol" value = {currSymbol} onChange={handleSymbolChange}/>
             </label>
+            <label className = "w-1/2">
+                <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Percentage" type = "text" name = "symbol" value = {currPercentage} onChange={handlePercentageChange}/>
+            </label>
             <div className ="flex justify-end w-1/2"><button className = "block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-auto" type = "button" onClick = {addStock}>Add Stock To Portfolio</button></div>
             <label className = "w-1/2">
-                <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Initial Balance" type = "text" name = "symbol" value = {initialBalance} onChange={handleBalanceChange}/>
+                <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Initial Balance" type = "text" name = "initialBalance" value = {initialBalance} onChange={handleBalanceChange}/>
             </label>
             <label className = "w-1/2">
-                <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Start Date" type = "text" name = "symbol" value = {selectedDate} onChange={handleDateChange}/>
+                <input className ="block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-full" placeholder = "Start Date" type = "text" name = "date" value = {selectedDate} onChange={handleDateChange}/>
             </label>
             <div className ="flex justify-end w-1/2"><button className = "block bg-offWhite border-grey rounded border-2 text-gray-700 text-sm font-bold mb-2 w-auto" type = "submit">Submit</button></div>
         </form>
+
+        //Add Output to the right of form to display current stock allocations.
     );
 };
 
