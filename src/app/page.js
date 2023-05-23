45d0861948726e0a7828cbfcce8d1d408c7454f7
Andrew Heim <59150695+andepants@users.noreply.github.com>
2023-05-23 13:57:11 -0600

    Revert "Input david"

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import InputForm from '../components/InputForm'
 import APIComponent from './APIComponent'
 import TradingDataTable from '../components/TradingDataTable'
 const tradingData = {
@@ -105,7 +104,6 @@ export default async function Home() {
   return (
      <main>
       <h1 className="flex m-5 justify-center text-4xl font-bold">Portfolio Calculator</h1>
-      <InputForm />
       {/* We can then pass the props { symbol, allocation, date_from, date_to, initialBalance } to the APIComponent here */}
       <APIComponent />
       <TradingDataTable tradingData={tradingData} userInputData={userInputData} />
