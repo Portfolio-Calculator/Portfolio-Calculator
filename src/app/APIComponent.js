20c5b7c801566ae341c562364a5924d97736d2fa
Andrew Heim <andrewsheim@gmail.com>
2023-05-23 13:10:43 -0600

    improved data flow, remove console.logs

--- src/app/APIComponent.js
+++ src/app/APIComponent.js
@@ -17,7 +17,6 @@ async function getDates(symbol, date_from, date_to, allocation, initialBalance) 
     return obj;
   }, {});
 
-  console.log(combinedObject);
 
   for (let i = 0; i < data.data.length; i++) {
     let eachDataPoint = data.data[i];
@@ -48,22 +47,18 @@ async function getDates(symbol, date_from, date_to, allocation, initialBalance) 
     };
   };
   finalData["portfolioAllocation"] = portfolioAllocation;
-  console.log('finalData', finalData, 'stringified finalData', JSON.stringify(finalData));
-  return data;
+  // console.log('finalData', finalData, 'stringified finalData', JSON.stringify(finalData));
+  return finalData;
 }
 
 export default async function APIComponent({ symbol = 'AAPL,GOOGL', date_from = '2023-01-25', date_to = '2023-01-31', allocation = [0.5, 0.5], initialBalance = 1000 }) {
   const data = await getDates(symbol, date_from, date_to, allocation, initialBalance);
+  console.log('data, this is where you would get the data from Divya', data);
   // object with keys {open, high, low, close, volume, adj_high, adj_low, adj_close, adj_open, adj_volume, split_factor, dividend, symbol, exchange, date})
 
   return (
     <main>
-      <h1>This is the component for getting the data</h1>
-      <p>Most likely the data visualization stuff will go here</p>
-      <div>SYMBOL, OPEN PRICE, DATE</div>
-      {data?.data.map((data, index) => (
-        <div key={index}>{data.symbol} + {data.open} + {data.date} </div>
-      ))}
+      <h1>Data visualization HERE</h1>
     </main>
   )
 }
